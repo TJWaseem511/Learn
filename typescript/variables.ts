e4c674626521c02b8035e9fb5fbf5e70cb992b3b
Waseem <tjwasim@gmail.com>
2018-11-30 17:15:16 +0530

    About variables

---
+++ typescript/variables.ts
@@ -0,0 +1,9 @@
+//Variable declaration in typescript
+
+// 1. Basic declaration
+//Syntax
+// var <variable_name>[= <value>];
+
+var a = 10;
+//above example creates a variable named a & assign value 10 to it
+console.log(10);
